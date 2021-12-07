c6d7d25542b4cf6d6182a2b1c16a1d1fe332a498
杨杰铭 <yangjm@mctech.vip>
2021-12-07 11:11:39 +0800

    clpk

---
+++ ColorPickerWin/ColorPickerWin/Controls/Primitive/ButtonBaseEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorPickerWin.Controls.Primitive
+{
+    /// <summary>
+    /// 使用自定义形状的button
+    /// </summary>
+    public class ButtonBaseEx : Control
+    {
+        static ButtonBaseEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ButtonBaseEx), new FrameworkPropertyMetadata(typeof(ButtonBaseEx)));
+        }
+    }
+}
